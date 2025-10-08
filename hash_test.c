83f6fab89a9db36ce660350c6620004dd66a471a
Said Kadrioski <said@kadrioski.de>
2025-10-08 03:52:43 +0200

    Add brackets to macro.

--- hash_test.c
+++ hash_test.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 #define MOD_FOR_TYPES(a, t) (a & ((sizeof(t) << 3) - 1))
-#define ELEMENTS(a) sizeof a / sizeof a[0]
+#define ELEMENTS(a) (sizeof a / sizeof a[0])
 
 unsigned long
 tiny_hash(char *str)
